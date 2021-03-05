055b8884984e7892426b104ce1d6fe4687462e02
Dana Farhat <dm.farhatt@gmail.com>
2021-03-05 11:36:42 -0500

    Initial Commit

---
+++ GitHub account/GitHub_accountApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_accountApp.swift
+//  GitHub account
+//
+//  Created by Dana Farhat on 2021-03-05.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_accountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
